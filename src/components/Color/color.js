e942d843679599139bdf8c006294fec4e5ffb2ae
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-30 06:38:30 -0700

    Add files via upload

--- src/components/Color/color.js
+++ src/components/Color/color.js
@@ -5,7 +5,13 @@ class Color extends Component {
     render() {
         return (
             <div className="color">
-                <div className={`colorBackground ${this.props.size}`} style={{background: this.props.color}}></div>
+                <div 
+                    className={`colorBackground ${this.props.size}`} 
+                    style={{background: this.props.color}}
+                    onClick={this.props.getColor.bind(null, this.props.color)}
+                >
+
+                </div>
                 <div className="colorName">{this.props.color}</div>
             </div>
         )
