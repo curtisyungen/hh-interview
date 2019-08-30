cc36e2a6999d30c4bea8d5bcfde6392ab0e10929
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-30 06:03:18 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ class App extends Component {
     let characters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 
     for (var code=0; code<100; code++) {
+      let code = "";
       for (var char=0; char<6; char++) {
-        let code = "";
         let randIdx = Math.floor(Math.random() * characters.length);
-        code += characters(randIdx);
+        code += characters[randIdx];
       }
 
       hexCodes.push(code);
