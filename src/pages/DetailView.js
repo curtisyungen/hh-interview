a29ea4c6b0a8f7becf24fd09b9435a9da29515bf
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-30 07:31:49 -0700

    Add files via upload

---
+++ src/pages/DetailView.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Color from "../components/Color/color";
+// import "./DetailView.css";
+
+class DetailView extends Component {
+    render() {
+        return (
+            <div>
+                <Color
+                    color={this.props.color}
+                    getColor={this.props.getColor}
+                    size={"large"}
+                />
+
+                {/* CLEAR BUTTON */}
+                <button
+                    className="btn btn-outline-dark clearBtn"
+                    onClick={this.props.clearDisplay}
+                >
+                    Clear
+                </button>
+            </div>
+        )
+    }
+}
+
+export default DetailView;
